f9e6ea9fb8189fd01a7e543ea0742a0c28684a91
Geoffrey Otieno <otienogeoffrey812@gmai.com>
2023-05-30 21:30:35 +0300

    Added Student Model

---
+++ src/main/java/com/example/Spring/Boot/With/Spring/Data/Redis/And/Jedis/model/Student.java
@@ -0,0 +1,15 @@
+package com.example.Spring.Boot.With.Spring.Data.Redis.And.Jedis.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class Student implements Serializable {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String mobileNumber;
+    private int age;
+}
